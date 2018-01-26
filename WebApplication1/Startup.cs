4528477aa86a9bb559aeb3ac05534be2e6c53a89
Jim Yarbro <yarbro@outlook.com>
2018-01-26 12:54:18 +0100

    Prep for publish.

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,45 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApplication1
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace WebApplication1 {
+	public class Startup {
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		public void ConfigureServices(IServiceCollection services) {
+			// This line is the magic that loads the values from appsettings.json into a ViewConfiguration object.
+			services.Configure<ViewConfiguration>(Configuration.GetSection("ViewConfiguration"));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+			services.AddMvc();
+		}
 
-            app.UseStaticFiles();
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else {
+				app.UseExceptionHandler("/Error");
+			}
 
-            app.UseMvc();
-        }
-    }
+			app.UseStaticFiles();
+
+			app.UseMvc();
+		}
+	}
 }
